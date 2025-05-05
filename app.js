8266ddb8a09e03bf862d43246f4aee206cad2429
JackGrein <145626910+JackGrein@users.noreply.github.com>
2025-05-05 06:11:33 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 function alertButton() {
     alert("Hello World!");
 }
+// Get current filename from URL
+const currentPage = window.location.pathname.split("/").pop();
+
+// Highlight matching nav link
+const navLinks = document.querySelectorAll('.nav-links a');
+navLinks.forEach(link => {
+  if (link.getAttribute('href') === currentPage) {
+    link.classList.add('active');
+  }
+});
